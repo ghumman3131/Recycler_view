6a5d748494cac6e8ae086cb79df532041b2dff57
charan ghumman <charanghumman@gmail.com>
2017-04-14 21:28:38 +0530

    php file for test data

---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+echo '[{"order_id":"1","user_id":"1","order_type":null,"date":"2017-04-14","time":"06:48:21 am"},{"order_id":"2","user_id":"1","order_type":null,"date":"2017-04-14","time":"06:48:57 am"},{"order_id":"3","user_id":"1","order_type":null,"date":"2017-04-14","time":"06:50:09 am"},{"order_id":"4","user_id":"1","order_type":null,"date":"2017-04-14","time":"06:51:46 am"},{"order_id":"5","user_id":"1","order_type":null,"date":"2017-04-14","time":"06:52:35 am"},{"order_id":"6","user_id":"1","order_type":null,"date":"2017-04-14","time":"06:53:24 am"},{"order_id":"7","user_id":"1","order_type":null,"date":"2017-04-14","time":"06:57:20 am"},{"order_id":"8","user_id":"1","order_type":null,"date":"2017-04-14","time":"06:58:33 am"},{"order_id":"9","user_id":"1","order_type":null,"date":"2017-04-14","time":"06:59:05 am"}]';
+
+?>
